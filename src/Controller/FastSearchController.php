297d3837ac8b2f6ed385a121d20fd34643e0b375
Bilal YILMAZ <mail@bilalyilmaz.com>
2022-11-23 09:34:04 +0300

    Toplam listelenen veri bilgisi

--- src/Controller/FastSearchController.php
+++ src/Controller/FastSearchController.php
@@ -34,6 +34,7 @@ class FastSearchController extends Controller
 
         $limitPerPage = $entityInfos[$key]['limit_per_page'] ?: $limitPerPage;
         $records      = json_decode(file_get_contents($filePath), true);
+        $all          = count($records);
         if (!$records) $records = [];
         $records      = $fss->filterRecords($records, $content['filters'], $entityInfos[$key]['filters']);
         $records      = $fss->sort($records, $content['sort_field'], $content['sort_direction']);
@@ -45,7 +46,7 @@ class FastSearchController extends Controller
         $eventDispatcher->dispatch(NetlivaFastSearchEvents::BEFORE_VIEW, $event);
 
 
-        return new JsonResponse(['records'=>$event->getRecords(), 'loaded' => $limitPerPage * $page, 'total' => $total]);
+        return new JsonResponse(['records'=>$event->getRecords(), 'loaded' => $limitPerPage * $page, 'total' => $total, 'all_count' => $all]);
     }
 
 
